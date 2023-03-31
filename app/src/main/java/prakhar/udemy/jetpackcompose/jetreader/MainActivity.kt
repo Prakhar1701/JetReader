f6aab572e239b706c721ec00406908510d5089a6
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-03-31 16:34:44 +0530

    Saved A User To Firestore Database - Test User

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/MainActivity.kt
@@ -1,6 +1,7 @@
 package prakhar.udemy.jetpackcompose.jetreader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import prakhar.udemy.jetpackcompose.jetreader.ui.theme.JetReaderTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,8 +19,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetReaderTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Prakhar"
+                user["lastName"] = "Agrawal"
+
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FB", "onCreate: ${it.id}")
+                        }
+                        .addOnFailureListener {
+                            Log.d("FB", "onCreate: ${it}")
+                        }
+
                     Greeting("Android")
                 }
             }
