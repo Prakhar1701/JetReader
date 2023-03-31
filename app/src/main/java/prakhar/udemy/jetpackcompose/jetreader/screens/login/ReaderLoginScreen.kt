425d760a281df5de17f457e9c1a14dc66bf0f839
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-01 00:07:25 +0530

    Created the User Form Composable

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/login/ReaderLoginScreen.kt
@@ -1,12 +1,24 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import prakhar.udemy.jetpackcompose.jetreader.components.JetReaderLogo
 
@@ -19,5 +31,31 @@ fun ReaderLoginScreen(navController: NavController) {
         ) {
             JetReaderLogo()
         }
+    }
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun UserForm() {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
+    val modifier = Modifier                     //For keyboard to not overlap views:)
+        .height(250.dp)
+        .background(MaterialTheme.colors.background)
+        .verticalScroll(rememberScrollState())
+
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
     }
 }
