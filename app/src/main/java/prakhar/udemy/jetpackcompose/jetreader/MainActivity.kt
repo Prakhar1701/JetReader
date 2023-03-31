b1ea4717997c44237a771b3b6da7d77579c3c3a2
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-03-31 20:38:05 +0530

    Using the Navigation and Showing the SplashScreen:)

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
+import prakhar.udemy.jetpackcompose.jetreader.navigation.ReaderNavigation
 import prakhar.udemy.jetpackcompose.jetreader.ui.theme.JetReaderTheme
 
 @AndroidEntryPoint
@@ -21,43 +27,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetReaderTheme {
-
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap()
-                user["firstName"] = "Prakhar"
-                user["lastName"] = "Agrawal"
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                    db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener {
-                            Log.d("FB", "onCreate: ${it.id}")
-                        }
-                        .addOnFailureListener {
-                            Log.d("FB", "onCreate: ${it}")
-                        }
-
-                    Greeting("Android")
-                }
+                ReaderApp()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReaderApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ReaderNavigation()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetReaderTheme {
-        Greeting("Android")
-    }
+    JetReaderTheme {}
 }
--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/ReaderSplashScreen.kt
@@ -1,8 +1,10 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderSplashScreen(navController: NavController) {
+    Text(text = "SplashScreen")
 }
