d6f14d0ea705f956b69ab09d0dc1bfc7b3dc8a48
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-03-31 19:44:40 +0530

    ReaderScreen Enum & App Routing Set Up

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/navigation/ReaderScreens.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/navigation/ReaderScreens.kt
@@ -1,4 +1,27 @@
 package prakhar.udemy.jetpackcompose.jetreader.navigation
 
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    ReaderStatsScreen;
+
+    companion object {
+        fun fromRoute(route: String): ReaderScreens = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            ReaderHomeScreen.name -> ReaderHomeScreen
+            SearchScreen.name -> SearchScreen
+            DetailScreen.name -> DetailScreen
+            UpdateScreen.name -> UpdateScreen
+            ReaderStatsScreen.name -> ReaderStatsScreen
+            null -> ReaderHomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
 }
