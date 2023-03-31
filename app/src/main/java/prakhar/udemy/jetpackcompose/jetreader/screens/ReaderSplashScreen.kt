87c6b3cd78aeb5a65904101e1731e4739a61db97
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-03-31 22:56:23 +0530

    Animated the Splash Screen

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/ReaderSplashScreen.kt
@@ -1,5 +1,9 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.magnifier
@@ -8,23 +12,38 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
 fun ReaderSplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(durationMillis = 800, easing = {
+                OvershootInterpolator(8f).getInterpolation(it)
+            })
+        )
+        delay(2000L)
+    }
     Surface(
         modifier = Modifier
             .padding(15.dp)
-            .size(
-                330.dp
-            ),
+            .size(330.dp)
+            .scale(scale.value),  //For Launch Effect
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
