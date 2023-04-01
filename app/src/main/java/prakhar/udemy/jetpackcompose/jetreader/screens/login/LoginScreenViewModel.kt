6f339ecd5afaad43a6cebd8c5183b12475713d3c
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-01 20:34:39 +0530

    Saving a User to Firestore Database when Users are Created

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/login/LoginScreenViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
@@ -49,6 +50,11 @@ class LoginScreenViewModel : ViewModel() {
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
+                        //--------------------------------------------------------------------------
+                        //prakhar@me.com -> prakhar
+                        val displayName = task.result?.user?.email?.split('@')?.get(0)
+                        createUser(displayName)
+                        //--------------------------------------------------------------------------
                         home()
                     } else {
                         Log.d("FB", "createUserWithEmailAndPassword: ${task.result.toString()}")
@@ -56,6 +62,16 @@ class LoginScreenViewModel : ViewModel() {
                     _loading.value = false
                 }
         }
+    }
+
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = mutableMapOf<String, Any>()
+        user["user_id"] = userId.toString()
+        user["display_name"] = displayName.toString();
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
     }
 //--------------------------------------------------------------------------------------------------
 }
