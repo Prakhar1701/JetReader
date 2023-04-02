a7027bafc8245c110c9278a995aeb38cf939e3ac
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-02 11:54:53 +0530

    Finishing up the Top Section - Reading and User Profile

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/home/ReaderHomeScreen.kt
@@ -1,9 +1,11 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,12 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 import prakhar.udemy.jetpackcompose.jetreader.components.ReaderAppBar
 import prakhar.udemy.jetpackcompose.jetreader.model.MBook
+import prakhar.udemy.jetpackcompose.jetreader.navigation.ReaderScreens
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -81,12 +86,42 @@ fun FABContent(onTop: () -> Unit) {
 @Preview
 @Composable
 fun HomeContent(navController: NavController = NavController(LocalContext.current)) {
+    val email = FirebaseAuth.getInstance().currentUser?.email
+    val currentUserName = if (!email.isNullOrEmpty()) {
+        //prakhar@me.com -> prakhar
+        FirebaseAuth.getInstance().currentUser?.email?.split('@')?.get(0)
+    } else "N/A"
     Column(
         Modifier.padding(2.dp),
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Row(modifier = Modifier.align(alignment = Alignment.Start)) {
+
             TitleSection(label = "Your reading \n " + " activity right now...")
+
+            Spacer(modifier = Modifier.fillMaxWidth(0.7f))
+            Column {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate(ReaderScreens.ReaderStatsScreen.name)
+                        }
+                        .size(45.dp),
+                    tint = MaterialTheme.colors.secondaryVariant)
+
+                Text(
+                    text = currentUserName!!, //!! -> Null Check
+                    modifier = Modifier.padding(2.dp),
+                    style = MaterialTheme.typography.overline,
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip
+                )
+                Divider()
+            }
         }
     }
 }
--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/stats/ReaderStatsScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/stats/ReaderStatsScreen.kt
@@ -1,8 +1,10 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens.stats
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun ReaderStatsScreen(navController: NavController){
+fun ReaderStatsScreen(navController: NavController) {
+    Text(text = "Stats...")
 }
