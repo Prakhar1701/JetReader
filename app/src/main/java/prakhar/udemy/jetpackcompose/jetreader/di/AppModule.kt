6cda1285b8f74e4d76c2e8ac7546c050e3af34e6
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-03 16:34:10 +0530

    Retrofit and Hilt DI SetUp

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/di/AppModule.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/di/AppModule.kt
@@ -1,10 +1,24 @@
 package prakhar.udemy.jetpackcompose.jetreader.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import prakhar.udemy.jetpackcompose.jetreader.screens.search.network.BooksApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideBooksApi(): BooksApi {
+        return Retrofit.Builder()
+            .baseUrl(prakhar.udemy.jetpackcompose.jetreader.utils.Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(BooksApi::class.java)
+    }
 }
---
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/utils/Constants.kt
@@ -0,0 +1,6 @@
+package prakhar.udemy.jetpackcompose.jetreader.utils
+
+object Constants {
+    // https://www.googleapis.com/books/v1/volumes?q=android
+    const val BASE_URL = "https://www.googleapis.com/books/v1/"
+}
