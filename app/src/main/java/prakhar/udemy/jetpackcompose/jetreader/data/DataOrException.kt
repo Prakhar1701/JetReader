adf13c1e385f8d4762047647e0fa2a89bb580b3b
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-05 00:49:45 +0530

    Revert "Repository and Data Wrapper Class SetUp"
    This reverts commit 5857b50a02393c9e100de51b3b9b4e6e6cf32577.

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/data/DataOrException.kt
+++
@@ -1,7 +0,0 @@
-package prakhar.udemy.jetpackcompose.jetreader.data
-
-data class DataOrException<T, Boolean, E : Exception>(
-    var data: T? = null,
-    var loading: Boolean? = null,
-    var e: E? = null
-)
