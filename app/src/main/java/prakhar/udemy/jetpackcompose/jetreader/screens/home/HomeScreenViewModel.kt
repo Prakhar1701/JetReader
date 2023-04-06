355807d9702139ff00e857946a3f19729985a224
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-06 17:44:53 +0530

    Retrieving Saved Books From FireStore Database Using HomeScreenViewModel

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/home/HomeScreenViewModel.kt
@@ -1,10 +1,34 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens.home
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import prakhar.udemy.jetpackcompose.jetreader.data.DataOrException
+import prakhar.udemy.jetpackcompose.jetreader.model.MBook
 import prakhar.udemy.jetpackcompose.jetreader.repository.FireRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeScreenViewModel @Inject constructor(private val repository: FireRepository) :
-    ViewModel() {}
+    ViewModel() {
+    val data: MutableState<DataOrException<List<MBook>, Boolean, Exception>> = mutableStateOf(
+        DataOrException(listOf(), true, Exception(""))
+    )
+
+    init {
+        getAllBooksFromDatabase()
+    }
+
+    private fun getAllBooksFromDatabase() {
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repository.getAllBooksFromDatabase()
+            if (!data.value.data.isNullOrEmpty()) data.value.loading = false
+        }
+        Log.d("GET", "getAllBooksFromDatabase: ${data.value.data?.toList().toString()}")
+    }
+}
