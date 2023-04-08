b9426667f62ec341531939f6d5a1a09b9d448b48
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-04-08 23:01:00 +0530

    Setting Up the Update Screen

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/update/ReaderBookUpdateScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetreader/screens/update/ReaderBookUpdateScreen.kt
@@ -1,15 +1,26 @@
 package prakhar.udemy.jetpackcompose.jetreader.screens.update
 
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import prakhar.udemy.jetpackcompose.jetreader.components.ReaderAppBar
+import prakhar.udemy.jetpackcompose.jetreader.data.DataOrException
+import prakhar.udemy.jetpackcompose.jetreader.model.MBook
 import prakhar.udemy.jetpackcompose.jetreader.screens.home.HomeScreenViewModel
 
 @Composable
@@ -29,6 +40,38 @@ fun BookUpdateScreen(
             navController.popBackStack()
         }
     }) {
-        Surface(modifier = Modifier.padding(it)) {}
+
+        val bookInfo = produceState<DataOrException<List<MBook>,
+                Boolean,
+                Exception>>(
+            initialValue = DataOrException(
+                data = emptyList(),
+                true, Exception("")
+            )
+        ) {
+            value = viewModel.data.value
+        }.value
+
+        Surface(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .padding(3.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(top = 3.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Log.d("INFO", "BookUpdateScreen: ${viewModel.data.value.data.toString()}")
+                if (bookInfo.loading == true) {
+                    LinearProgressIndicator()
+                    bookInfo.loading = false
+                } else {
+                    Text(text = viewModel.data.value.data?.get(0)?.title.toString()) //Working some time else crashing :(
+                }
+            }
+        }
+
     }
 }
